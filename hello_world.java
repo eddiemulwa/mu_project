eeb440c16ce3763525a84203b694f181307b1841
eddiemu <eddiemu@gmail.com>
2015-03-05 23:45:12 -0800

    This is a new file for testing git commands.

---
+++ hello_world.java
@@ -0,0 +1,14 @@
+// This is a test java code file
+
+// I will not add code at this time
+public class hello_world{
+
+	public hello_world{
+
+	}
+
+	public static void main(sString[] args){
+		System.out.println('Hello World!');
+	}
+}
+
